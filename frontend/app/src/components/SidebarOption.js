b365529f77fb68c311294b9e88776fa54e54ef32
AritriBiswas <aritribiswas07@gmail.com>
2022-06-11 21:34:11 +0530

    made the chat body

--- frontend/app/src/components/SidebarOption.js
+++ frontend/app/src/components/SidebarOption.js
@@ -2,10 +2,17 @@ import React from "react";
 import styled from "styled-components"
 
 
-function SidebarOptions(props){
+function SidebarOptions(Icon ,title){
     return(
         <SidebarOptionContainer>
             {Icon && <Icon fontSize = "small" style={{padding : 10}}/>}
+            {Icon ? (
+                <h3>{title}</h3>
+            ):(
+                <SidebarOptionChannel>
+                    <span>#</span>{title}
+                </SidebarOptionChannel>
+            ) }
         </SidebarOptionContainer>
     );
 }
